77d334745782e7e223faf0da16b477141803835a
yrcosmos.69@gmail.com <madao@MADAO.Dlink>
2025-01-22 20:28:40 +0530

    added concurrency to handle multiple clients

--- LoadBalancer.cpp
+++ LoadBalancer.cpp
@@ -1,5 +1,45 @@
 #include "server.h"
 
+void handleClient(int clientSocket){
+	//send/receive
+		char buffer[4096] = {0};
+		int bytes_recv = recv(clientSocket,buffer,sizeof(buffer)-1,0);
+		if(bytes_recv <= 0){
+			std::cerr<<"Failed to receive data or client disconnected\n";
+			return;
+		}
+		buffer[bytes_recv] = '\0';
+		std::cout<<"Request Received:\n";
+		std::cout<<buffer<<"\n";
+
+		//send request to backend server
+		int sockbe = socket(AF_INET,SOCK_STREAM,0);
+
+		sockaddr_in servaddr;
+		servaddr.sin_family = AF_INET;
+		servaddr.sin_port = htons(BEPORT);
+		inet_pton(AF_INET,BEIP,&servaddr.sin_addr);
+
+		if(connect(sockbe,(sockaddr*)&servaddr,sizeof(servaddr)) == -1){
+			std::cout<<"Error in connecting to the backend server";
+			return;
+		}
+
+		write(sockbe,buffer,bytes_recv);
+		memset(buffer,0,sizeof(buffer));
+		int bebytes = read(sockbe,buffer,sizeof(buffer));
+		buffer[bebytes] = '\0';
+
+		std::cout<<"from Server:\n"<<buffer<<"\n";
+
+		//forward response from backend server to the client;
+		send(clientSocket,buffer,bebytes,0);
+
+		close(sockbe);
+
+		close(clientSocket);
+}
+
 int main(int argc, char* argv[]){
 
 	std::cout<<"Load Balancer started!\n";
@@ -35,66 +75,38 @@ int main(int argc, char* argv[]){
 	socklen_t clientsize = sizeof(client);
 	char host[NI_MAXHOST];
 	char svc[NI_MAXSERV];
+	int clientSocket;
 
-	int clientSocket = accept(sock, (sockaddr*)&client, &clientsize);
+	std::vector<std::thread> threads;
 
-	if(clientSocket == -1){
-		std::cerr<<"Problem with client connection";
-		return 4;
+	while(true){
+		clientSocket = accept(sock, (sockaddr*)&client, &clientsize);
+
+		if(clientSocket == -1){
+			std::cerr<<"Problem with client connection";
+			return 4;
+		}
+
+		memset(host, 0, NI_MAXHOST);
+		memset(svc, 0, NI_MAXSERV);
+
+		int result = getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV, 0);
+
+		if(result){
+			std::cout<<host<<" Connected on "<<svc<<std::endl;
+		}else{
+			inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+			std::cout<<host<<" Connected on "<<ntohs(client.sin_port)<<std::endl;
+		}
+
+		
+		threads.emplace_back(std::thread(handleClient,clientSocket));
+		threads.back().detach();
+
 	}
-
-	close(sock);
-
-	memset(host, 0, NI_MAXHOST);
-	memset(svc, 0, NI_MAXSERV);
-
-	int result = getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV, 0);
-
-	if(result){
-		std::cout<<host<<" Connected on "<<svc<<std::endl;
-	}else{
-		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-		std::cout<<host<<" Connected on "<<ntohs(client.sin_port)<<std::endl;
-	}
-
-	//send/receive
-	char buffer[4096] = {0};
-	int bytes_recv = recv(clientSocket,buffer,sizeof(buffer)-1,0);
-	if(bytes_recv <= 0){
-		std::cerr<<"Failed to receive data or client disconnected\n";
-		return 5;
-	}
-	buffer[bytes_recv] = '\0';
-	std::cout<<"Request Received:\n";
-	std::cout<<buffer<<"\n";
-
-	//send request to backend server
-	int sockbe = socket(AF_INET,SOCK_STREAM,0);
-
-	sockaddr_in servaddr;
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(BEPORT);
-	inet_pton(AF_INET,BEIP,&servaddr.sin_addr);
-
-	if(connect(sockbe,(sockaddr*)&servaddr,sizeof(servaddr)) == -1){
-		std::cout<<"Error in connecting to the backend server";
-		return 6;
-	}
-
-	write(sockbe,buffer,bytes_recv);
-	memset(buffer,0,sizeof(buffer));
-	int bebytes = read(sockbe,buffer,sizeof(buffer));
-	buffer[bebytes] = '\0';
-
-	std::cout<<"from Server:\n"<<buffer<<"\n";
-
-	//forward response from backend server to the client;
-	send(clientSocket,buffer,bebytes,0);
-
-	close(sockbe);
 
 	//close
-	close(clientSocket);
-
+	close(sock);
+	
 	return 0;
 }
--- server.h
+++ server.h
@@ -5,10 +5,14 @@
 #include <string.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <thread>
+#include <vector>
 
 #define SERVERIP 	"0.0.0.0"
 #define BEIP		"127.0.0.1"
 #define LBPORT		80
 #define BEPORT		8080
+
+void handleClient(int);
 
 int main(int, char**);
