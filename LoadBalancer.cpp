eee1c9392e752ef3207f9734f6e55ebe1c468dbe
yrcosmos.69@gmail.com <madao@MADAO.Dlink>
2025-01-25 13:22:20 +0530

    Step 3 complete: Periodic server health check implemented

--- LoadBalancer.cpp
+++ LoadBalancer.cpp
@@ -1,8 +1,75 @@
 #include "server.h"
 
+bool getSetHealth(int id,bool val = true,bool get = true){
+	healthmtx.lock();
+	if(get){
+		healthmtx.unlock();
+		return health[id];
+	}
+	health[id] = val;
+	healthmtx.unlock();
+	return val;
+}
+
+int sendReqServer(int beport, int& sockbe,char* buffer,int bytes_recv){
+	mtx.lock();
+		sockaddr_in servaddr;
+		servaddr.sin_family = AF_INET;
+		servaddr.sin_port = htons(beport);
+		inet_pton(AF_INET,BEIP,&servaddr.sin_addr);
+
+		if(connect(sockbe,(sockaddr*)&servaddr,sizeof(servaddr)) == -1){
+			mtx.unlock();
+			return 0;
+		}
+
+		std::cout<<"connected to server"<<BEIP<<":"<<beport<<"\n";
+		std::cout<<"Sending message to server:\n"<<buffer<<"\n";
+
+		write(sockbe,buffer,bytes_recv);
+		memset(buffer,0,MXSIZE);
+		int bebytes = 0, total_bytes = 0;
+		while ((bebytes = read(sockbe, buffer + total_bytes, MXSIZE - total_bytes - 1)) > 0) {
+    		total_bytes += bebytes;
+    		if (total_bytes >= MXSIZE - 1) break; // Prevent buffer overflow
+		}
+		buffer[total_bytes] = '\0';
+		std::cout<<"Response from server:\n"<<buffer<<"\n";
+
+		mtx.unlock();
+		return total_bytes;
+}
+
+void healthCheck(){
+	//iterate through the serverlist 
+	//check each server by sending an http get request
+	//update health status in health list
+	//wait the mentioned amount of time
+	//repeat
+
+	while(true){
+		for(int i = 0;i<servnum;i++){
+			char buffer[MXSIZE] = "GET / HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n";				//put get request message here
+			int bytes_recv = 54;
+			buffer[bytes_recv] = '\0';
+			int beport = servlist[i];
+			int sockbe = socket(AF_INET,SOCK_STREAM,0);
+			int bebytes = sendReqServer(beport,sockbe,buffer,bytes_recv);
+			if(!bebytes){
+				//it means the server is not well
+				getSetHealth(i,false,false);
+			}else{
+				getSetHealth(i,true,false);
+				std::cout<<"Health report of server"<<BEIP<<":"<<beport<<":\n"<<buffer<<"\n";
+			}
+		}
+		std::this_thread::sleep_for(std::chrono::seconds(10));
+	}
+}
+
 void handleClient(int clientSocket){
 	//send/receive
-		char buffer[4096] = {0};
+		char buffer[MXSIZE] = {0};
 		int bytes_recv = recv(clientSocket,buffer,sizeof(buffer)-1,0);
 		if(bytes_recv <= 0){
 			std::cerr<<"Failed to receive data or client disconnected\n";
@@ -12,29 +79,30 @@ void handleClient(int clientSocket){
 		std::cout<<"Request Received:\n";
 		std::cout<<buffer<<"\n";
 
-		//send request to backend server
+		int chk = servit;
+		while(!getSetHealth(servit)){
+			servit++;
+			servit %=servnum;
+			if(chk == servit){
+				break;
+			}
+		}
+
+		int beport = servlist[servit];
 		int sockbe = socket(AF_INET,SOCK_STREAM,0);
 
-		sockaddr_in servaddr;
-		servaddr.sin_family = AF_INET;
-		servaddr.sin_port = htons(servlist[servit]);
-		inet_pton(AF_INET,BEIP,&servaddr.sin_addr);
-
-		//switch to another server number
-		servit++;
-		servit = servit%servnum;
-
-		if(connect(sockbe,(sockaddr*)&servaddr,sizeof(servaddr)) == -1){
+		//send request to backend server
+		int bebytes = sendReqServer(beport,sockbe,buffer,bytes_recv);
+		if(bebytes == 0){
 			std::cout<<"Error in connecting to the backend server";
 			return;
 		}
 
-		write(sockbe,buffer,bytes_recv);
-		memset(buffer,0,sizeof(buffer));
-		int bebytes = read(sockbe,buffer,sizeof(buffer));
-		buffer[bebytes] = '\0';
+		//switch to another port number
+		servit++;
+		servit %=servnum;
 
-		std::cout<<"from Server:\n"<<buffer<<"\n";
+		// std::cout<<"from Server:\n"<<buffer<<"\n";
 
 		//forward response from backend server to the client;
 		send(clientSocket,buffer,bebytes,0);
@@ -46,6 +114,12 @@ void handleClient(int clientSocket){
 
 int main(int argc, char* argv[]){
 	servnum = servlist.size();
+	health.resize(servnum);
+	fill(health.begin(),health.end(),true);
+
+	//start a background task of health check
+	std::thread Health = std::thread(healthCheck);
+	Health.detach();
 
 	std::cout<<"Load Balancer started!\n";
 
@@ -104,7 +178,7 @@ int main(int argc, char* argv[]){
 			std::cout<<host<<" Connected on "<<ntohs(client.sin_port)<<std::endl;
 		}
 
-		
+
 		threads.emplace_back(std::thread(handleClient,clientSocket));
 		threads.back().detach();
 
--- server.h
+++ server.h
@@ -7,15 +7,28 @@
 #include <netdb.h>
 #include <thread>
 #include <vector>
+#include <chrono>
+#include <mutex>
 
 #define SERVERIP 	"0.0.0.0"
 #define BEIP		"127.0.0.1"
 #define LBPORT		80
 #define BEPORT		8080
+#define INTERVAL	10000
+#define MXSIZE		8192
 
-std::vector<int> servlist{8080,8081};
+std::vector<int> servlist{8080,8081,8082};
+std::vector<bool> health;
 int servnum;
 int servit=0;
+std::mutex mtx;
+std::mutex healthmtx;
+
+bool getSetHealth(int,bool,bool);
+
+int sendReqServer(int,int&,char*,int);
+
+void healthCheck();
 
 void handleClient(int);
 
